85e12f60e199c3b2a44903f6b2fa938c7d2987e5
MishaNikasov <mishanikasov@mail.ru>
2017-11-08 23:35:09 +0300

    Add sout.

--- app/src/main/java/donnu/nikasov/musicnews/Main/MainActivity.java
+++ app/src/main/java/donnu/nikasov/musicnews/Main/MainActivity.java
@@ -57,6 +57,8 @@ public class MainActivity extends AppCompatActivity
 
         System.out.println("GIT");
 
+        System.out.println("GIT");
+
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Галерея");
         getSupportActionBar().hide();
